a2734ea1c2347188901b5622cb2267c600435f86
Sandra Machon <55002027+sandramariamachon@users.noreply.github.com>
2021-04-18 13:23:43 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# -*- coding: cp1250 -*-
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -11,25 +11,7 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
 
-
-
-
-
 ########### Define your variables
-'''
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Test'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
-'''
 myheading='Coffee Ranking by Origin'
 tabtitle= 'Coffee Ranking by Origin'
 data_coffee = pd.DataFrame(pd.read_csv('http://raw.githubusercontent.com/sandramariamachon/coffe_dashboard_heroku/master/datasets_coffee/data.csv'))
@@ -40,34 +22,8 @@ data_coffee = data_coffee.drop([ 'altitude_high_meters','altitude_low_meters', '
 test = data_coffee.groupby(['country','code_3'])[["Total.Cup.Points",'Aroma','Flavor','Aftertaste','Acidity','Body','Balance','Uniformity','Sweetness','Clean.Cup','Cupper.Points']].mean()
 test.reset_index(inplace=True)
 test = test.round(decimals=2)
-'''
-Brazil = test.loc[0]
-Brazil.apply(pd.to_numeric, errors='ignore')
 
-'''
-########### Set up the chart
-'''
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-'''
+########### Set up the chart of the world map
 for col in test.columns:
     test[col] = test[col].astype(str)
     
@@ -108,7 +64,7 @@ fig.update_layout(
     )] 
 )
 
-#########Table
+#########Set uo the table with best origins
 df = pd.read_csv('http://raw.githubusercontent.com/sandramariamachon/coffe_dashboard_heroku/master/datasets_coffee/table_board.csv')
 def generate_table(dataframe, max_rows=10):
     return html.Table([
@@ -123,16 +79,9 @@ def generate_table(dataframe, max_rows=10):
     ])
 
 #########Flavour Radar Chart
-##test
 countries = df['Country'].unique()
 
 characteristics = df.columns[1:]
-
-#Brazil = pd.DataFrame(dict(
-    #r=[7.55,7.57,7.44,7.51,7.54,7.53,9.88,9.95,9.85,7.56],
-    #theta=['Aroma','Flavour','Aftertaste','Acidity', 'Body','Balance','Uniformity','Sweetness','Clean.Cup','Cupper.Points']))
-
-#fig2 = px.line_polar(Brazil ,r='r', theta ='theta', line_close=True, color_discrete_sequence=px.colors.sequential.solar)
 
 # the style arguments for the main content page.
 CONTENT_STYLE = {
@@ -148,43 +97,7 @@ TEXT_STYLE = {
     'padding': '20px 10px'
 }
 
-
-
 ########### Set up the layout
-'''
-The layout is composed of a tree of "components" like html.Div and dcc.Graph.
-The dash_html_components library has a component for every HTML tag.
-The html.H1(children='Hello Dash')component generates
-a <h1>Hello Dash</h1> HTML element in your application.
-
-The children property is special. By convention,
-it's always the first attribute which means that you can omit it:
-html.H1(children='Hello Dash') is the same as html.H1('Hello Dash').
-Also, it can contain a string, a number, a single component, or a list of components.
-'''
-'''
-app.layout = html.Div(children=[
-html.H1(myheading),
-
-dcc.Graph(
-        id='example-graph',
-        figure=fig),
-
-
-html.Div(children=[html.H4(children='10 Top Coffee Origins'),
-    generate_table(df)]),
-
-
-dcc.Dropdown(
-        id='country_name',
-        options=[{'label': i, 'value': i} for i in countries],
-        value='Country'),
-
-dcc.Graph(id='radar_graph'),
-
-])
-'''
-
 title1 = dbc.Col([html.H2('The Map of Coffee Origins', style={
             'textAlign': 'center','color': '#704510','padding': '40px 10px'})])
 
@@ -197,19 +110,7 @@ title2 = dbc.Col([html.H2('Highest Rated Origins', style={
 
 table = dbc.Col([dbc.Table.from_dataframe(df.head(n=5), striped=True, bordered=True, hover=True, responsive=True)])
 
-
-'''
-dbc.Row(
-    [
-        dbc.Col(
-        html.Div(children=[html.H2(children='10 Top Coffee Origins', style={
-            'textAlign': 'center','color': '#191950'
-        },),
-        generate_table(df)]), md=12)])
-
-'''
-
-
+#set up the jumbotron table explaining the coffee characteristics
 jumbotron = dbc.Jumbotron(
     [
         html.H1("Welcome to the Coffee Origins interactive dashboard!", style={'color': '#704510'}, className="display-3"),
@@ -231,24 +132,11 @@ jumbotron = dbc.Jumbotron(
         dbc.ListGroupItem("Balance - The balance represents complexity. How do all the different attributes play together, and does it deepen the coffee?"),
         dbc.ListGroupItem("Uniformity - Uniformity refers to the consistency of flavour of the different cups of the sample tasted. "),
         dbc.ListGroupItem("Sweetness - Sweetness refers to a pleasing fullness of flavour as well as any obvious sweetness and its perception is the result of the presence of certain carbohydrates."),
-        dbc.ListGroupItem("Clean Cup - Clean Cup refers to a lack of interfering negative impressions from first ingestion to final aftertaste, a “transparency” of cup")]),
+        dbc.ListGroupItem("Clean Cup - Clean Cup refers to a lack of interfering negative impressions from first ingestion to final aftertaste, a ï¿½transparencyï¿½ of cup")]),
         html.P(
-            "The Final Score is calculated by summing the individual scores given for each of the primary attributes in the box marked “Total Cup Points.”", style={'color': '#704510', 'padding': '20px 10px'})
+            "The Final Score is calculated by summing the individual scores given for each of the primary attributes in the box marked ï¿½Total Cup Points.ï¿½", style={'color': '#704510', 'padding': '20px 10px'})
         
 ])
-
-#title3 = dbc.Col([html.H2('Taste profile by Origin', style={
-            #'textAlign': 'center','color': '#704510','padding': '40px 10px'})])
-
-#titles= html.Div([
-    #dbc.Row(
-           #dbc.Col(html.Div(html.H2('Taste profile by Origin', style={
-            #'textAlign': 'center','color': '#704510','padding': '40px 10px'})),width=True),
-           # dbc.Col(html.Div(html.H2('Quality Measures Bar Chart', style={
-           # 'textAlign': 'center','color': '#704510','padding': '40px 10px'})),width = True), align="center",)])
-
-
-
 
 dropdown = html.Div(
     [
@@ -267,38 +155,6 @@ row = html.Div(
 ])
 
 
-'''
-controls= dbc.FormGroup(
-    [
-        dcc.Dropdown(
-            id='country_name',
-            options=[{
-                'label': i,
-                'value': i} for i in countries],value = 'Hawaii')])
-'''
-#row = html.Div(
-    #dbc.Row(
-            #[
-            #dbc.Col(html.Div(dcc.Graph(id='radar_graph'))),
-            #dbc.Col(html.Div(dcc.Graph(id='taste_graph')))]))
-
-
-#radar_graph = dbc.Row([dbc.Col(dcc.Graph(id='radar_graph'), width={"size": 4, "offset": 3})])
-
-
-#title4 = dbc.Col([html.H2('Quality Measures Bar Chart', style={
-            #'textAlign': 'center','color': '#704510','padding': '40px 10px'})])
-'''
-dropdown= dbc.FormGroup(
-    [
-        dcc.Dropdown(
-            id='characteristic_name',
-            options=[{
-                'label': i,
-                'value': i} for i in characteristics],value = 'Total Cup Points')])
-'''
-#taste_graph = dbc.Row([dbc.Col(dcc.Graph(id='taste_graph'), width={"size": 10, "offset": 1})])
-
 content = html.Div(
     [
         html.H2('Coffee Origins Dashboard', style=TEXT_STYLE),
@@ -308,21 +164,13 @@ content = html.Div(
         map_chart,
         title2,
         table,
-        #title3,
-        #controls,
-        #radar_graph,
-        #title4 ,
-        #titles,
         dropdown,
-        #taste_graph,
         row
     ],
     style=CONTENT_STYLE
 )
 
 ########### Initiate the app
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app = dash.Dash(external_stylesheets=[dbc.themes.SKETCHY])
 app.layout = html.Div(content)
 server = app.server
@@ -342,19 +190,14 @@ def update_graph(_country):
             countryIndex = i 
         i = i + 1
           
-    Zorg = pd.DataFrame(dict(
-    r=[df.iloc[countryIndex][2],df.iloc[countryIndex][3],df.iloc[countryIndex][4],df.iloc[countryIndex][5],df.iloc[countryIndex][6],df.iloc[countryIndex][7],df.iloc[countryIndex][8],df.iloc[countryIndex][9]],
+    Coffee_Data = pd.DataFrame(dict(
+    r=[df.iloc[countryIndex][2],df.iloc[countryIndex][3],
+    df.iloc[countryIndex][4],df.iloc[countryIndex][5],
+    df.iloc[countryIndex][6],df.iloc[countryIndex][7],
+    df.iloc[countryIndex][8],df.iloc[countryIndex][9]],
     theta=["Aroma",'Flavor','Aftertaste','Acidity', 'Body','Balance','Uniformity','Sweetness']))
-    fig2 = px.line_polar(Zorg ,r='r', theta ='theta', range_r = [0,10], line_close=True, color_discrete_sequence=px.colors.sequential.solar)
-    #please rewrite this so you don't hardcode anything!
-
-    #fig2 = px.line_polar(df['Country'],
-                     #r='r', theta='theta',
-                     #line_close=True, color_discrete_sequence=px.colors.sequential.solar)
-
-    #fig2.update_layout(margin={'l': 60, 'b': 60, 't': 10, 'r': 0}, hovermode='closest')
-
-
+    fig2 = px.line_polar(Coffee_Data ,r='r', theta ='theta', range_r = [0,10], line_close=True, color_discrete_sequence=px.colors.sequential.solar)
+  
     return fig2
 
 @app.callback(
@@ -366,11 +209,6 @@ def update_graph(_character):
     hover_data= [b], color=b,
     labels={'pop':b}, height=450, color_continuous_scale='Brwnyl_r') 
     return graph
-
-
-
-
-
 
 
 
